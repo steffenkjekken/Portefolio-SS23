94d3fad00e5ced78a224daf0fca4cd5b59c1218b
steffenkjekken <steffen.rolland@outlook.com>
2023-01-23 02:55:23 +0100

    submit page

---
+++ timer.js
@@ -0,0 +1,20 @@
+
+function Timer() {
+    var counter = 5;
+    var myTimer = setInterval(function() {
+      document.getElementById("timer").innerHTML = counter;
+      counter--;
+      if (counter < 0) {
+        clearInterval(myTimer);
+        window.location="index.html";
+      }
+
+      if (counter < 1) {
+        document.getElementById("second").innerHTML = "second"
+      }
+      
+    
+    }, 1000);
+  }
+
+Timer();
